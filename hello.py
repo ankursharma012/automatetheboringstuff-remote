3e6ec20861c294d3197abb150a502295674c8523
Ankur <ankursharma012@gmail.com>
2024-11-05 11:49:20 +1100

    Test the waters

---
+++ hello.py
@@ -0,0 +1,10 @@
+# Program to accept name and age
+
+print('Hello World')
+print('What is your name ?')
+myName = input("Type your name = ")
+print('It is good to meet you ' + myName)
+
+myAge = input('What is your age ? ')
+print('Your age is ' + myAge)
+print('Length of the name is ' + str(len(myName)) + ' Length of the age is ' + str(len(myAge)))
---
+++ test.py
@@ -0,0 +1,10 @@
+import random
+
+pets = ['cat', 'dog', 'mouse']
+
+print(random.choice(pets))
+
+print(pets)
+
+random.shuffle(pets)
+print(pets)
